ed59b4a5b7b2347db5b7c609a91829305c604ade
vagrant <vagrant@precise32.(none)>
2014-02-27 22:14:09 +0000

    lab8

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,7 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$("button.likeBtn").click(function(e) {
+		ga("send", "event", "Category", "Action");
+	});
 }
